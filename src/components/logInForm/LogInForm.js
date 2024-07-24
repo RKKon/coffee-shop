9df56a2ff10f6dd67e575516c6c63ea4e260613d
RKKon <rkkon@mail.ru>
2024-07-24 12:30:55 +0300

    Fixed reg form, css.

--- src/components/logInForm/LogInForm.js
+++ src/components/logInForm/LogInForm.js
@@ -3,29 +3,48 @@ import { useForm } from 'react-hook-form';
 import '../../assets/sass/style.sass';
 import '../registrationForm/registrationForm.sass';
 
-const LogInForm = ({toggleRegForm, toggleLogInForm}) => {
+const LogInForm = ({ toggleRegForm, toggleLogInForm }) => {
   const { register, handleSubmit } = useForm();
+
+  const setOverflowHidden = (boolean = false) => {
+    boolean ? document.body.style.overflow = 'hidden' : document.body.style.overflow = '';
+  }
+
   const openRegForm = () => {
     toggleRegForm(true);
-    toggleLogInForm(false)
+    toggleLogInForm(false);
   }
+
+  const closeFormByClickWrapper = (event) => {
+    if (event.target.classList.contains('registration_form_wrapper')) {
+      toggleLogInForm(false);
+      setOverflowHidden(false);
+    }
+  }
+
   return (
-    <div className="registration_form_wrapper">
+    <div onClick={(event) => closeFormByClickWrapper(event)} className="registration_form_wrapper">
+      {setOverflowHidden(true)}
       <form className="registration_form" onSubmit={handleSubmit((data) => {
         console.log(data);
         toggleLogInForm(false);
+        setOverflowHidden(false);
       })}
-      > 
-        <div onClick={() => toggleLogInForm(false)} className='registration_form_cross'>&times;</div>
+      >
+        <div className='registration_form_cross'
+          onClick={() => {
+            toggleLogInForm(false);
+            setOverflowHidden(false);
+          }} >&times;</div>
         <h2 className="registration_form_title">Sign up</h2>
-        <input {...register("username", {required: true, minLength: 2})} 
+        <input {...register("username", { required: true, minLength: 2 })}
           className="registration_form_item" type="text" placeholder="Username" />
-        <input {...register("password", {required: true, minLength: 5})} 
+        <input {...register("password", { required: true, minLength: 5 })}
           className="registration_form_item" type="text" placeholder="Password" />
-        <p onClick={openRegForm} className="registration_form_item registration_form_link"> 
-          Need to Sign up?
+        <p onClick={openRegForm} className="registration_form_item registration_form_link">
+          Need to Register?
         </p>
-        <button className="registration_form_btn">Register</button>  
+        <button className="registration_form_btn">Sing up</button>
       </form>
     </div>
   )
--- src/components/registrationForm/RegistrationForm.js
+++ src/components/registrationForm/RegistrationForm.js
@@ -3,33 +3,51 @@ import { useForm } from "react-hook-form";
 import '../../assets/sass/style.sass';
 import './registrationForm.sass';
 
-const RegistrationForm = ({toggleRegForm, toggleLogInForm}) => {
+const RegistrationForm = ({ toggleRegForm, toggleLogInForm }) => {
   const { register, handleSubmit } = useForm();
+
+  const setOverflowHidden = (boolean = false) => {
+    boolean ? document.body.style.overflow = 'hidden' : document.body.style.overflow = ''
+  }
+
   const openLogInForm = () => {
     toggleRegForm(false);
-    toggleLogInForm(true)
+    toggleLogInForm(true);
   }
+
+  const closeFormByClickWrapper = (event) => {
+    if (event.target.classList.contains('registration_form_wrapper')) {
+      toggleRegForm(false);
+      setOverflowHidden(false);
+    }
+  }
+
   return (
-    <div className="registration_form_wrapper">
+    <div onClick={(event) => closeFormByClickWrapper(event)} className="registration_form_wrapper">
+      {setOverflowHidden(true)}
       <form className="registration_form" onSubmit={handleSubmit((data) => {
         console.log(data);
         toggleRegForm(false);
+        setOverflowHidden(false);
       })}
-      > 
-        <div onClick={() => toggleRegForm(false)} className='registration_form_cross'>&times;</div>
+      >
+        <div className='registration_form_cross' onClick={() => {
+          toggleRegForm(false)
+          setOverflowHidden(false)
+        }}>&times;</div>
         <h2 className="registration_form_title">Register</h2>
-        <input {...register("username", {required: true, minLength: 2})} 
+        <input {...register("username", { required: true, minLength: 2 })}
           className="registration_form_item" type="text" placeholder="Username" />
-        <input {...register("password", {required: true, minLength: 5})} 
+        <input {...register("password", { required: true, minLength: 5 })}
           className="registration_form_item" type="text" placeholder="Password" />
         {/* <input {...register("repeatPassword", {required: true, minLength: 5})}
         className="registration_form_item" type="text" placeholder="Repeat password" /> */}
-        <input {...register("email", {required: true})} 
+        <input {...register("email", { required: true })}
           className="registration_form_item" type="text" placeholder="Email" />
-        <p onClick={openLogInForm} className="registration_form_link"> 
+        <p onClick={openLogInForm} className="registration_form_link">
           Already registered?
         </p>
-        <button className="registration_form_btn">Register</button>  
+        <button className="registration_form_btn">Register</button>
       </form>
     </div>
   )
